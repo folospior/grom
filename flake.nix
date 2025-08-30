a0cf1267545ce11bca3aa5d5170ea9d88ae62927
Filip Hoffmann <folosp@protonmail.com>
2025-08-31 00:47:16 +0200

    flake updates

--- flake.nix
+++ flake.nix
@@ -1,17 +1,33 @@
 {
-  inputs.nixpkgs.url = "nixpkgs/nixos-25.05";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    gleam.url = "github:Comamoca/gleam-overlay";
+  };
   outputs = {
     nixpkgs,
+    gleam,
     ...
   }: let
-    pkgs = import nixpkgs {system = "x86_64-linux";};
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ] (system:
+        function (import nixpkgs {
+          inherit system;
+          overlays = [gleam.overlays.default];
+        }));
   in {
-    devShells."x86_64-linux".default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        gleam
-        rebar3
-        beamPackages.erlang
-      ];
-    };
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShellNoCC {
+        buildInputs = with pkgs; [
+          pkgs.gleam.bin.latest
+          rebar3
+          beamPackages.erlang
+        ];
+      };
+    });
   };
 }
